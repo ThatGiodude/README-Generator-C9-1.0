03a71a2a3d80a7095f9ac3dee7b045e40bc446cf
ThatGiodude <giovannifstrangio@gmail.com>
2024-03-15 02:59:01 -0400

    testing the markdown functionality

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require('fs');
-const util = require("util");
 const generatorMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [{
@@ -66,7 +65,7 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.createPromptModule(questions)
+    inquirer.prompt(questions)
     .then(function(data) {
         writeToFile("README.md", generatorMarkdown(data));
     })
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license === 'BSD') {
-    return `![Static Badge](https://img.shields.io/badge/License-BSD-brightgreen)`
+  if (license) {
+    return `![License](https://img.shields.io/badge/license-$(license)-blue.svg)`;
+  } else {
+    return '';
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    return `\n* [license](#license)\n`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
