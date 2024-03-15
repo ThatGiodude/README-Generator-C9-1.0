d827c4829833d5accb9acbef425fb1631c9a73c7
ThatGiodude <giovannifstrangio@gmail.com>
2024-03-15 01:48:24 -0400

    added the date for the readme to create

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'BSD') {
+    return `![Static Badge](https://img.shields.io/badge/License-BSD-brightgreen)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,12 +12,46 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license || license === "none") {
+    return "";
+  } return `## License
+  This project is licensed under the [${license}](${renderLicenseLink(license)}) license.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  -[Installation](#Installation)
+  -[Usage](#Usage)
+  -[Contributing](#Contributing)
+  -[Tests](#Tests)
+  -[License Information](#License)
+  -[Questions](#Questions)
 
+  ## Installation
+  ${data.installation}
+
+  ##Usage
+  ${data.Usage}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+  ${data.Contribution}
+
+  ## Tests
+  ${data.Tests}
+
+  ## Questions
+  For additional questions, you can reach me through:
+
+  Github:[${data.github}](https://github.com/${data.github})
+
+  Email: ${data.email}
 `;
 }
 
